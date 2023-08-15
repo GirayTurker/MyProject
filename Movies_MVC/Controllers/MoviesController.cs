90f46feb13c486aa89fcb5046c424082223e14c3
GirayTurker <tug88418@temple.edu>
2023-08-15 11:42:45 -0400

    Action Results Commit

--- Movies_MVC/Controllers/MoviesController.cs
+++ Movies_MVC/Controllers/MoviesController.cs
@@ -13,7 +13,32 @@ namespace Movies_MVC.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+           return View(movie);
+           //return Content("Hello World");
+           //return HttpNotFound();
+           //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" }); // Shows on Browser as http://localhost:5000/?page=1&sortBy=name
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            //Fixed this section. HasValue function is printing only value 1 for pageIndex
+            const int a = 1;
+            if (pageIndex.Equals(null))
+            {
+                pageIndex = a;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex,sortBy));
         }
     }
 }
